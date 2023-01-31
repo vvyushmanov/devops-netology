ecdd280ef70e4b7192866a5c4435d86da493d1eb
Vadim Yushmanov <v_yushmanov@mail.ru>
2023-02-01 01:00:47 +0400

    7.2 init commit

---
+++ Virt/DZ_7_2_TF_Syntax/yc/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.85.0"
+  hashes = [
+    "h1:+xNw9gm61zPtv1ZF1jsDfUxU5ComHusdItirng907iY=",
+    "h1:3MBmptadpBzcbREAXEzratBMXPcZk1GZMEjERg+JVJg=",
+  ]
+}
---
+++ Virt/DZ_7_2_TF_Syntax/yc/main.tf
@@ -0,0 +1,25 @@
+provider "yandex" {}
+
+data "yandex_compute_image" "Ubuntu-22" {
+  family = "ubuntu-2204-lts"
+}
+
+resource "yandex_compute_instance" "Netology-Ubuntu-22" {
+  name = "netologyvyushmanov"
+  hostname = "vyushmanov"
+
+  boot_disk {
+    initialize_params {
+      image_id = "${data.yandex_compute_image.Ubuntu-22.id}"
+      size = 15
+    }
+  }
+  network_interface {   
+    subnet_id = "e9b20c9j4u08daq9vf8e" 
+  }
+
+  resources {
+    cores = 2
+    memory = 2
+  }
+}
