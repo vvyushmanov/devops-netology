1d680a87a26ba96f4d36fa6fe283890a6116a6df
Vadim Yushmanov <v_yushmanov@mail.ru>
2023-10-12 07:31:50 +0400

    jsonnets

---
+++ 99-Diploma/35-k8s/monitoring/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+# Make sure to use project tooling
+PATH="$(pwd)/tmp/bin:${PATH}"
+
+kubectl apply --server-side -f manifests/setup
+kubectl wait \
+	--for condition=Established \
+	--all CustomResourceDefinition \
+	--namespace=monitoring
+kubectl apply -f manifests/
