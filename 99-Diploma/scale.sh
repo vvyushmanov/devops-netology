ad2434a79fb88e8707743fe8295a1e5eb0910064
Vadim Yushmanov <v_yushmanov@mail.ru>
2023-10-18 19:13:58 +0400

    added "scale" script

---
+++ 99-Diploma/scale.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+root=$(pwd)
+
+# Terraform
+cd $root/10-terraform
+terraform apply -auto-approve
+cp ./hosts.yml $root/20-kubespray/inventory/diploma/
+mainIP=$(terraform output -raw main-ip)
+
+echo "========================================="
+
+# Kubespray
+cd $root/20-kubespray
+ansible-playbook -i inventory/diploma/hosts.yml scale.yml -b -v --user=ubuntu
+echo "Wait for all kube-system components to be up"
+kubectl wait --for=condition=Ready pod -n kube-system --all --timeout=900s
+
+echo "========================================="
+
+
+echo "Congrats! Cluster is fully operational"
+echo "Open main page: http://$mainIP/"
+echo "Grafana: http://$mainIP/grafana/"
